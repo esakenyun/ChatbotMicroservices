3478f9a92dd33828547c1245923dbb8e04673c07
esakenyun <rfahlevih@gmail.com>
2024-06-20 14:32:38 +0700

    deletefrontend folde

--- Frontend/src/app/page.jsx
+++
@@ -1,5 +0,0 @@
-import HomePageComponent from "@/components/page/HomePageComponent";
-
-export default function Home() {
-  return <HomePageComponent />;
-}
--- Frontend/src/components/page/PromptPageComponent.jsx
+++
@@ -1,3 +0,0 @@
-export default function PromptPageComponent() {
-  return <div>PromptPageComponent</div>;
-}
